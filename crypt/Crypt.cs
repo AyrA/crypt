b4c0b1c51894b0924627bf30e58116f5796b5665
Kevin Gut <kevin.gut@bitm.sg>
2017-05-01 00:20:03 +0200

    Initial commit
    - Basic functionality is working

---
+++ crypt/Crypt.cs
@@ -0,0 +1,192 @@
+﻿using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace crypt
+{
+    public class Crypt
+    {
+        public const string HASHALG = "SHA256";
+        public const string HEADER = "ACRYPT";
+
+        public static bool EncryptFile(string Filename, string DestinationName, string Password)
+        {
+            if (IsEncrypted(Filename))
+            {
+                File.Move(Filename, DestinationName);
+            }
+            else
+            {
+                using (Rijndael R = Rijndael.Create())
+                {
+                    R.GenerateIV();
+                    int KeySize = R.LegalKeySizes.OrderByDescending(m => m.MaxSize).First().MaxSize / 8;
+                    byte[] Salt = RandomBytes(KeySize);
+                    byte[] Key = DeriveBytes(Password, KeySize, Salt);
+                    byte[] Hash = GerPasswordByteHash(Key);
+                    using (var IN = File.OpenRead(Filename))
+                    {
+                        using (var FS = File.Create(DestinationName))
+                        {
+                            //Write Header
+                            using (var BW = new BinaryWriter(FS, Encoding.UTF8, true))
+                            {
+                                BW.Write(Encoding.UTF8.GetBytes(HEADER));
+                                BW.Write(Salt.Length);
+                                BW.Write(Salt);
+                                BW.Write(R.IV.Length);
+                                BW.Write(R.IV);
+                                BW.Write(Hash.Length);
+                                BW.Write(Hash);
+
+                                BW.Flush();
+                            }
+                            using (CryptoStream CS = new CryptoStream(FS, R.CreateEncryptor(Key, R.IV), CryptoStreamMode.Write))
+                            {
+                                try
+                                {
+                                    IN.CopyTo(CS, R.BlockSize * 100);
+                                }
+                                catch
+                                {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool DecryptFile(string Filename, string DestinationName, string Password)
+        {
+            if (File.Exists(Filename) && !IsEncrypted(Filename))
+            {
+                File.Move(Filename, DestinationName);
+                return true;
+            }
+            using (Rijndael R = Rijndael.Create())
+            {
+                int KeySize = R.LegalKeySizes.OrderByDescending(m => m.MaxSize).First().MaxSize / 8;
+                R.GenerateIV();
+                using (var IN = File.OpenRead(Filename))
+                {
+                    byte[] Salt = null;
+                    byte[] IV = null;
+                    byte[] Hash = null;
+                    using (var BR = new BinaryReader(IN, Encoding.UTF8, true))
+                    {
+                        if (Encoding.UTF8.GetString(BR.ReadBytes(HEADER.Length)) != HEADER)
+                        {
+                            return false;
+                        }
+                        try
+                        {
+                            Salt = BR.ReadBytes(BR.ReadInt32());
+                            IV = BR.ReadBytes(BR.ReadInt32());
+                            Hash = BR.ReadBytes(BR.ReadInt32());
+                        }
+                        catch
+                        {
+                            return false;
+                        }
+                    }
+                    R.IV = IV;
+                    byte[] Key = DeriveBytes(Password, KeySize, Salt);
+
+                    if (!CheckPasswordBytes(Key, Hash))
+                    {
+                        return false;
+                    }
+
+                    using (var FS = File.Create(DestinationName))
+                    {
+                        try
+                        {
+                            using (CryptoStream CS = new CryptoStream(IN, R.CreateDecryptor(Key, R.IV), CryptoStreamMode.Read))
+                            {
+                                CS.CopyTo(FS, R.BlockSize * 100);
+                            }
+                        }
+                        catch
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static byte[] GerPasswordByteHash(byte[] Password)
+        {
+            using (var A = HashAlgorithm.Create(HASHALG))
+            {
+                return A.ComputeHash(Password);
+            }
+        }
+
+        private static bool CheckPasswordBytes(byte[] Password, byte[] Hash)
+        {
+            var H = GerPasswordByteHash(Password);
+            for (var i = 0; i < H.Length; i++)
+            {
+                if (H[i] != Hash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsEncrypted(string Filename)
+        {
+            var ret = File.Exists(Filename);
+            if (ret)
+            {
+                using (var FS = File.OpenRead(Filename))
+                {
+                    using (var BR = new BinaryReader(FS))
+                    {
+                        if (Encoding.UTF8.GetString(BR.ReadBytes(HEADER.Length)) != HEADER)
+                        {
+                            return false;
+                        }
+                        try
+                        {
+                            //Read 3 byte arrays from the file (IV,Salt,Hash)
+                            BR.ReadBytes(BR.ReadInt32());
+                            BR.ReadBytes(BR.ReadInt32());
+                            BR.ReadBytes(BR.ReadInt32());
+                        }
+                        catch
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private static byte[] RandomBytes(int Count)
+        {
+            byte[] Data = new byte[Count];
+            using (RNGCryptoServiceProvider RNG = (RNGCryptoServiceProvider)RandomNumberGenerator.Create())
+            {
+                RNG.GetBytes(Data);
+            }
+            return Data;
+        }
+
+        private static byte[] DeriveBytes(string Password, int NumberOfBytes, byte[] Salt, int Cycles = 50000)
+        {
+            using (var Deriver = new Rfc2898DeriveBytes(Password, Salt, Cycles))
+            {
+                return Deriver.GetBytes(NumberOfBytes);
+            }
+        }
+    }
+}
---
+++ crypt/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace crypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[] { "/d", "\\temp\\media\\cavestory" };
+            if (args.Length == 0 || args.Contains("/?"))
+            {
+                ShowHelp();
+            }
+            #region ENCRYPT
+            //process encryption part
+            else if (args.Select(m => m.ToLower()).Contains("/e"))
+            {
+                //get rid of '/e'
+                args = args.Where(m => m.ToLower() != "/e").ToArray();
+                if (args.Length > 0)
+                {
+                    string Source = Path.GetFullPath(args[0]);
+                    if (!File.Exists(Source) && !Directory.Exists(Source))
+                    {
+                        Console.Error.WriteLine($"{Source} is neither file nor directory");
+                        ShowHelp();
+                        return;
+                    }
+                    //Create a set of source and destination names
+                    string[] Sources = Directory.Exists(Source) ? Directory.GetFiles(Source, "*.*", SearchOption.AllDirectories) : new string[] { Source };
+                    Console.Error.WriteLine($"Ready to encrypt {Sources.Length} files");
+                    Console.Write("Password: ");
+                    string Pass = ReadPassword();
+                    if (string.IsNullOrEmpty(Pass))
+                    {
+                        Console.Error.WriteLine("Aborting on empty password");
+                        return;
+                    }
+                    foreach (var F in Sources)
+                    {
+                        Console.Error.Write($"{F}...");
+                        if (Crypt.EncryptFile(F, F + ".cry", Pass))
+                        {
+                            if (File.Exists(F))
+                            {
+                                File.Delete(F);
+                            }
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Error.WriteLine("[DONE]");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Error.WriteLine("[ERROR]");
+                        }
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine("Missing source file/directory argument");
+                    ShowHelp();
+                    return;
+                }
+            }
+            #endregion
+            #region DECRYPT
+            //process decryption part
+            else if (args.Select(m => m.ToLower()).Contains("/d"))
+            {
+                //get rid of '/d'
+                args = args.Where(m => m.ToLower() != "/d").ToArray();
+                if (args.Length > 0)
+                {
+                    string Source = Path.GetFullPath(args[0]);
+                    if (!File.Exists(Source) && !Directory.Exists(Source))
+                    {
+                        Console.Error.WriteLine($"{Source} is neither file nor directory");
+                        ShowHelp();
+                        return;
+                    }
+                    //Create a set of source and destination names
+                    string[] Sources = Directory.Exists(Source) ? Directory.GetFiles(Source, "*.cry", SearchOption.AllDirectories) : new string[] { Source };
+                    Console.Error.WriteLine($"Ready to decrypt {Sources.Length} files");
+                    Console.Write("Password: ");
+                    string Pass = ReadPassword();
+                    if (string.IsNullOrEmpty(Pass))
+                    {
+                        Console.Error.WriteLine("Aborting on empty password");
+                        return;
+                    }
+                    foreach (var F in Sources)
+                    {
+                        Console.Error.Write($"{F}...");
+                        if (Crypt.DecryptFile(F, F.Substring(0, F.Length - 4), Pass))
+                        {
+                            if (File.Exists(F))
+                            {
+                                File.Delete(F);
+                            }
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Error.WriteLine("[DONE]");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Error.WriteLine("[ERROR]");
+                        }
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine("Missing mode argument: /d or /e");
+                    ShowHelp();
+                    return;
+                }
+            }
+            #endregion
+            else
+            {
+                ShowHelp();
+            }
+        }
+
+        private static string ReadPassword()
+        {
+#if DEBUG
+            const string PASS="Test-1234567890";
+            Console.Error.WriteLine(string.Empty.PadRight(PASS.Length, '*'));
+            return PASS;
+#else
+            string pass = "";
+            while (true)
+            {
+                var Key = Console.ReadKey(true);
+                switch (Key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        Console.WriteLine();
+                        return pass;
+                    case ConsoleKey.Backspace:
+                        if (pass.Length > 0)
+                        {
+                            pass = pass.Substring(0, pass.Length - 1);
+                            Console.Write("\b \b");
+                        }
+                        else
+                        {
+                            Console.Beep();
+                        }
+                        break;
+                    default:
+                        if (!char.IsControl(Key.KeyChar) && pass.Length < 40)
+                        {
+                            pass += Key.KeyChar;
+                            Console.Write('*');
+                        }
+                        else
+                        {
+                            Console.Beep();
+                        }
+                        break;
+                }
+            }
+#endif
+        }
+
+        private static void ShowHelp()
+        {
+            Console.Error.WriteLine(@"crypt {/e|/d} <input>
+
+/e      - Encrypt the supplied file/directory
+/d      - Decrypt the supplied file/directory
+input   - Input file/directory");
+        }
+    }
+}
